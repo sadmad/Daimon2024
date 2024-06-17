eb4ec7884d53b4581b950e66a433bfdd6221b5b3
Sarmad <sarmad.re@gmail.com>
2024-06-17 14:09:23 +0200

    The link is not working

--- src/home/3block/secBlock.jsx
+++ src/home/3block/secBlock.jsx
@@ -1,13 +1,10 @@
+import { Link } from "react-router-dom";
 import "./secBlock.css";
 import interreg from "../../../images/interreg_baltic_sea_region.jpg";
 import amucad from "../../../images/AmuCad_logo_crown_blue_500.png";
 import daimon from "../../../images/daimon2.png";
 const SecBlock = () => {
-    let img = [
-        interreg,
-        amucad,
-        daimon
-      ];
+  let img = [interreg, amucad, daimon];
   return (
     <>
       <div className="container-block d-flex align-items-center justify-content-center position-relative flex-wrap">
@@ -18,14 +15,21 @@ const SecBlock = () => {
           <div className="content">
             <h2>Interreg</h2>
             <p>
-            Decision Aid for Marine Munitions: Practical Application
-            was an international applied-science project consisting of partners from Poland, Germany, Sweden, Finland and Norway collaborating with experts worldwide, united by the goal of solving the problem of underwater munitions. Its budget was roughly 900k EUR and was part-financed by the EU INTERREG Baltic Sea Region Programme 2014-2020.
+              Decision Aid for Marine Munitions: Practical Application was an
+              international applied-science project consisting of partners from
+              Poland, Germany, Sweden, Finland and Norway collaborating with
+              experts worldwide, united by the goal of solving the problem of
+              underwater munitions. Its budget was roughly 900k EUR and was
+              part-financed by the EU INTERREG Baltic Sea Region Programme
+              2014-2020.
             </p>
-            <a className="btn btn-outline-secondary" 
+            <a
+              className="btn btn-outline-secondary"
               href="https://interreg-baltic.eu/"
               target="_blank"
               rel="noopener noreferrer"
-              role="button">
+              role="button"
+            >
               Read More
             </a>
           </div>
@@ -37,13 +41,17 @@ const SecBlock = () => {
           <div className="content">
             <h2>Ammunition Cadastre Sea</h2>
             <p>
-              EGEOS participates in national and international research projects on submerged munitions. Results from these projects are integrated into the "Ammunition Cadastre Sea" (AMUCAD).
+              EGEOS participates in national and international research projects
+              on submerged munitions. Results from these projects are integrated
+              into the "Ammunition Cadastre Sea" (AMUCAD).
             </p>
-            <a className="btn btn-outline-secondary" 
+            <a
+              className="btn btn-outline-secondary"
               href="https://www.amucad.org/"
               target="_blank"
               rel="noopener noreferrer"
-              role="button">
+              role="button"
+            >
               Read More
             </a>
           </div>
@@ -55,15 +63,17 @@ const SecBlock = () => {
           <div className="content">
             <h2>Daimon 2</h2>
             <p>
-            The DAIMON2 project optimized tools for maritime and environmental authorities, enabling more accurate and cost-effective decisions on managing dumped munitions.
+              The DAIMON2 project optimized tools for maritime and environmental
+              authorities, enabling more accurate and cost-effective decisions
+              on managing dumped munitions.
             </p>
-            <a className="btn btn-outline-secondary" 
-              href="https://interreg-baltic.eu/"
-              target="_blank"
-              rel="noopener noreferrer"
-              role="button">
+            <Link
+              className="btn btn-outline-secondary"
+              to="/DaimonPage"
+              role="button"
+            >
               Read More
-            </a>
+            </Link>
           </div>
         </div>
       </div>
