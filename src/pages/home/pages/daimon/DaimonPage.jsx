c9b70a4320a80ee9c313996e2ef66c9bb7102e92
Sa <sa@dblab3.in.tu-clausthal.de>
2024-06-28 12:40:56 +0200

    The logo

--- src/pages/home/pages/daimon/DaimonPage.jsx
+++ src/pages/home/pages/daimon/DaimonPage.jsx
@@ -21,6 +21,8 @@ const DaimonPage = () => {
   const { ref: ref3, inView: inView3 } = useInView(options);
   const controls = useAnimation();
   const [showContent, setShowContent] = useState(false);
+  const titleControl = useAnimation();
+  const parControl = useAnimation();
 
   useEffect(() => {
     const sequence = async () => {
@@ -34,6 +36,18 @@ const DaimonPage = () => {
         transform: "translate(0, 0)",
         transition: { duration: 3, ease: "easeInOut" },
       });
+      await titleControl.start({
+        opacity: 1,
+        top: "35%",
+        transform: "translate(0, 0)",
+        transition: { duration: 3, ease: "easeInOut" },
+      });
+      await parControl.start({
+        opacity: 1,
+        top: "50%",
+        transform: "translate(0, -20%)",
+        transition: { duration: 3, ease: "easeInOut" },
+      });
       setShowContent(true);
     };
 
@@ -43,22 +57,22 @@ const DaimonPage = () => {
   return (
     <Container fluid>
       <Row className="vh-100">
-        <Col xs={12} className="text-center">
-          <motion.div 
+        <Col xs={10} className="text-center">
+          <motion.div
             id="position-animation"
             initial={{ opacity: 0, top: "50%" }}
             animate={controls}
-            style={{  }}
+            style={{}}
           >
             <Row className="first-animated-row align-items-center justify-content-center">
-              <Col xs={12} md={1}>
+              <Col xs={5} md={1}>
                 <img
                   src={ML}
                   alt="TU Clausthal-Machine learning"
                   className="img-fluid ml"
                 />
               </Col>
-              <Col xs={12} md={4}>
+              <Col xs={5} md={4}>
                 <img
                   className="img-fluid daimon-image"
                   src={daimonNew}
@@ -68,43 +82,52 @@ const DaimonPage = () => {
             </Row>
           </motion.div>
           <motion.div
-              className="project-name"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, transition: { duration: 2 } }}
-            >
-              <Row><Col><h2>Decision Aid for Marine Munitions: Practical Application</h2></Col></Row>
-            </motion.div>
+            className="project-name"
+            initial={{ opacity: 0, top: "50%" }}
+            animate={titleControl}
+            style={{ position: "absolute", top: "50%" }}
+          >
+            <Row>
+              <Col>
+                <h3>
+                  Decision Aid for Marine Munitions: Practical Application
+                </h3>
+              </Col>
+            </Row>
+          </motion.div>
+
+          <motion.div
+            className="project-explanation"
+            initial={{ opacity: 0 }}
+            animate={parControl}
+            style={{ position: "absolute"}}
+          >
+            <Col>
+              <Row>
+                <p>
+                  The Baltic Sea harbors a grim legacy of approximately 50,000
+                  tons of chemical warfare agents and at least 200,000 tons of
+                  conventional munitions from World Wars I and II. Additionally,
+                  around 45,000 tons of CWAs and 200,000 tons of conventional
+                  munitions were dumped off the coast of Norway in Skagerrak,
+                  with another 20,000 tons of CWAs near Måseskär, Sweden. These
+                  underwater munitions constantly release contaminants due to
+                  shell corrosion and human disturbances. The DAIMON project
+                  (2016-2019), with a budget of 4.7 million EUR and
+                  part-financed by the EU INTERREG Baltic Sea Region Programme,
+                  addressed the contentious issue of how to manage these
+                  underwater warfare objects. They developed decision-aid tools
+                  to assist Baltic Sea Region governments and companies in
+                  making informed, case-specific decisions.
+                </p>
+              </Row>
+            </Col>
+          </motion.div>
         </Col>
       </Row>
 
-
       {showContent && (
         <>
-          <Row className="flex-grow-1 align-items-center justify-content-center">
-            
-            <motion.div
-              className="project-explanation"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, transition: { duration: 2, delay: 1 } }}
-            >
-              <p>
-                The Baltic Sea harbors a grim legacy of approximately 50,000
-                tons of chemical warfare agents and at least 200,000 tons of
-                conventional munitions from World Wars I and II. Additionally,
-                around 45,000 tons of CWAs and 200,000 tons of conventional
-                munitions were dumped off the coast of Norway in Skagerrak, with
-                another 20,000 tons of CWAs near Måseskär, Sweden. These
-                underwater munitions constantly release contaminants due to
-                shell corrosion and human disturbances. The DAIMON project
-                (2016-2019), with a budget of 4.7 million EUR and part-financed
-                by the EU INTERREG Baltic Sea Region Programme, addressed the
-                contentious issue of how to manage these underwater warfare
-                objects. They developed decision-aid tools to assist Baltic Sea
-                Region governments and companies in making informed,
-                case-specific decisions.
-              </p>
-            </motion.div>
-          </Row>
           {/* Additional content rows */}
           <Row className="my-5">
             <Col ref={ref1}>
