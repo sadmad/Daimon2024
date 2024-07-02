1243acf8577bf5a005bc49ee3d143f0d31100384
Sa <sa@dblab3.in.tu-clausthal.de>
2024-07-02 15:40:44 +0200

    daimon page is finished!

--- src/pages/home/pages/daimon/DaimonPage.jsx
+++ src/pages/home/pages/daimon/DaimonPage.jsx
@@ -7,7 +7,8 @@ import "./DaimonPage.css";
 import ML from "../../../../images/machine-learning-daimon-page.svg";
 import daimonNew from "../../../../images/daimon-new-logo.png";
 import useScrollToTopOnReload from "../../../../components/useSccrollToTopOnReload";
-import dss from "../../../../images/dss.png";
+import posts from "./posts" //array of the post at the end of the page
+import PostGenerator from "../../components/postGenerator/PostGenerator"
 
 const DaimonPage = () => {
   useScrollToTopOnReload();
@@ -16,9 +17,9 @@ const DaimonPage = () => {
     threshold: 0.1,
   };
 
-  const { ref: ref1, inView: inView1 } = useInView(options);
+/*   const { ref: ref1, inView: inView1 } = useInView(options);
   const { ref: ref2, inView: inView2 } = useInView(options);
-  const { ref: ref3, inView: inView3 } = useInView(options);
+  const { ref: ref3, inView: inView3 } = useInView(options); */
   const controls = useAnimation();
   const [showContent, setShowContent] = useState(false);
   const titleControl = useAnimation();
@@ -141,145 +142,19 @@ const DaimonPage = () => {
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 4 }}
+          transition={{ duration: 1 }}
         >
           {/* Additional content rows */}
-          <Row className="my-0 justify-content-md-center">
-            <Col md={7} ref={ref1}>
-              <Row>
-                <Col>
-                  <motion.div
-                    initial={{ opacity: 0, x: -100 }}
-                    animate={
-                      inView1 ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }
-                    }
-                    transition={{ duration: 0.5 }}
-                  >
-                    <h3>Decision Support System (DSS) for marine munitions </h3>
-                  </motion.div>
-                </Col>
-              </Row>
-              <motion.div
-                initial={{ opacity: 0, y: 100 }}
-                animate={
-                  inView1 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }
-                }
-                transition={{ delay:1, duration: 0.5 }}
-              >
-                <p>
-                  The DAIMON project, running from 2016 to 2019 with a budget of
-                  4.7 million EUR and part-financed by the EU INTERREG Baltic
-                  Sea Region Programme, tackled the pressing issue of underwater
-                  munitions in the Baltic Sea. With approximately 50,000 tons of
-                  chemical warfare agents and at least 200,000 tons of
-                  conventional munitions from World Wars I and II dumped in the
-                  Baltic Sea, the project sought to address the ongoing release
-                  of contaminants due to corrosion and human disturbance. DAIMON
-                  developed a suite of decision-aid tools to assist governments
-                  and companies in making informed decisions about marine
-                  munitions management. These tools included a Decision Support
-                  System (DSS), a comprehensive munitions database, an EcoTox
-                  Toolbox for detecting warfare agents, and various management
-                  strategies. The follow-up project, DAIMON 2, aimed to
-                  popularize these tools and train administrative end-users,
-                  ensuring widespread practical application and enhancing the
-                  safety and environmental health of the Baltic Sea region.
-                </p>
-              </motion.div>
-            </Col>
-            <Col md={2}>
-              <img src={dss} alt="Example" />
-            </Col>
-          </Row>
-          <Row className="my-1 justify-content-md-center">
-            <Col md={7} ref={ref2}>
-              <Row>
-                <Col>
-                  <motion.div
-                    initial={{ opacity: 0, x: 100 }}
-                    animate={
-                      inView2 ? { opacity: 1, x: 0 } : { opacity: 0, x: 100 }
-                    }
-                    transition={{ duration: 0.5 }}
-                  >
-                    <h3>Baltic Sea Munitions Database</h3>
-                  </motion.div>
-                </Col>
-              </Row>
-              <motion.div
-                initial={{ opacity: 0, y: 100 }}
-                animate={
-                  inView2 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }
-                }
-                transition={{ delay:1, duration: 0.5 }}
-              >
-                <p>
-                  The Clausthal University of Technology, in collaboration with
-                  Dr. Koehler GmbH, has developed a specialist software designed
-                  to manage complex data about munitions objects in the Baltic
-                  Sea. This software allows munitions experts, scientists, and
-                  practitioners involved in Baltic Sea exploration to
-                  conveniently input, retrieve, or update information on newly
-                  discovered objects through a user-friendly interface. The
-                  resulting Munitions Database securely stores detailed
-                  information on dumped munitions and is hosted within the
-                  integrated oceanographic system ZSPDO. Available free of
-                  charge to registered users, this database can be accessed
-                  through the provided link. For login rights or further
-                  inquiries, please contact Prof. Dr. Sven Hartmann at the
-                  Clausthal University of Technology.
-                </p>
-              </motion.div>
-            </Col>
-            <Col md={2}>
-              <img src="https://via.placeholder.com/400" alt="Example" />
-            </Col>
-          </Row>
-          <Row className="my-2 justify-content-md-center">
-            <Col md={7} ref={ref3}>
-              <Row>
-                <Col>
-                  <motion.div
-                    initial={{ opacity: 0, y: 100 }}
-                    animate={
-                      inView3 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }
-                    }
-                    transition={{ duration: 0.5 }}
-                  >
-                    <h3>Catalogue of Baltic Sea dumped munitions' types</h3>
-                  </motion.div>
-                </Col>
-              </Row>
-              <motion.div
-                initial={{ opacity: 0, y: 100 }}
-                animate={
-                  inView3 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }
-                }
-                transition={{delay:1, duration: 0.5 }}
-              >
-                <p>
-                  The Polish Naval Academy in Gdynia, in collaboration with
-                  other DAIMON partners, has developed an online catalogue of
-                  all types of munitions resting on the Baltic Sea floor.
-                  Registered users can access and filter data through
-                  multi-attribute database queries. The catalogue, accessible
-                  here, covers both chemical and conventional munitions dumped
-                  after World War II. It aims to enhance knowledge about these
-                  munitions and identify pieces found during economic activities
-                  such as fishing, construction, and dredging. Users can search
-                  for munitions based on weapon type, calibre, and country of
-                  origin, with additional detailed information available in
-                  separate documents where applicable. For login rights or
-                  further inquiries, please contact Bartłomiej Pączek. For GIS
-                  information system access, including DAIMON and CHEMSEA
-                  results, use the provided link.
-                </p>
-              </motion.div>
-            </Col>
-            <Col md={2}>
-              <img src="https://via.placeholder.com/400" alt="Example" />
-            </Col>
-          </Row>
+          
+        {posts.map((post, index)=> (
+          <PostGenerator
+            key={index}
+            title={post.title}
+            img={post.img}
+            alt={post.alt}
+            text={post.text} />
+        ))}
+          
         </motion.div>
       )}
     </Container>
