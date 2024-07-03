6d501a1e3baa375cc9edc47a09a6b7a7e6d76f1b
sadmad <sarmad.re@gmail.com>
2024-07-03 10:53:34 +0200

    it is a test

--- src/pages/home/components/footer/Footer.jsx
+++ src/pages/home/components/footer/Footer.jsx
@@ -56,7 +56,7 @@ export default function Footer() {
             </MDBCol>
 
             <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+              <h6 className="text-uppercase fw-bold mb-4">just a test</h6>
               <p>
                 <a href="#!" className="text-reset">
                   Angular
